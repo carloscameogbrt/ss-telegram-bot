5ef479ba6b42566fa032eb52bdc9817635810ab6
carloscameogbrt <carloscamegbrt@gmail.com>
2020-07-25 18:43:39 +0200

    Final TFM commit

--- ss-bot.py
+++ ss-bot.py
@@ -10,22 +10,28 @@ import datetime
 import os
 import logging
 import logging.config
+import boto3
+from botocore.exceptions import NoCredentialsError
 
 from suntime import Sun, SunTimeException
 from timezonefinder import TimezoneFinder
 import pytz
 
-TOKEN = "1290123918:AAHozYmKK3QNZ8dGMdkvaswYDHaNPUovqJE" #os.environ['TOKEN']
+TOKEN = #os.environ['TOKEN']
 URL = "https://api.telegram.org/bot{}/".format(TOKEN)
 DOWNLOAD_URL = "https://api.telegram.org/file/bot{}/".format(TOKEN)
+TUTORIAL_URL = "https://github.com/carloscameogbrt/ss-telegram-bot/blob/master/assets/"
+
+#S3
+ACCESS_KEY = #os.environ['ACCESSKEYS3']
+SECRET_KEY = #os.environ['SECRETKEYS3']
+BUCKET_BASE = 'https://tfm-telegram-bot.s3.eu-central-1.amazonaws.com/'
 
 
 locations = dict()
-logging.config.fileConfig('ss-telegram-bot-master/config/logging.cfg')  # logfile config, logging.config.fileConfig('config/logging.cfg') si fuera de heroku
+logging.config.fileConfig('ss-telegram-bot/config/logging.cfg')  # logfile config, logging.config.fileConfig('config/logging.cfg') si fuera de heroku
 
 status = dict()
-
-time_zone = None
 
 observations = dict()
 
@@ -91,6 +97,7 @@ def send_message(text, chat_id, reply_markup=None):
         url += "&reply_markup={}".format(reply_markup)
     get_url(url)
 
+#Las fotos del tutorial cargan desde el github
 def send_photo(caption, photo, chat_id, reply_markup=None):
     caption.replace(" ","+")
     url = URL + "sendPhoto?caption={}&photo={}&chat_id={}".format(caption, photo, chat_id)
@@ -100,16 +107,15 @@ def send_photo(caption, photo, chat_id, reply_markup=None):
 
 
 keyboard_wait = ['Let\'s go!','Más info sobre el proyecto','Denunciar incidencia','Salir']
-keyboard_menu = ['Let\'s go!', 'Classify', 'About','Help']
+keyboard_menu = ['Let\'s go!', 'Classify', 'About','Help', 'Privacy policy']
 keyboard_tutorial = ['Start tutorial','Exit']
 keyboard_tutorial_step1 = ['Tip 1','Exit']
 keyboard_tutorial_step2 = ['Tip 2','Exit']
-keyboard_tutorial_finish = ['Finish']
-keyboard_new = ['Send a new one', "Finish"]
+keyboard_tutorial_finish = ['Exit']
+keyboard_new = ["Send a new observation of the same type as the previous one", "Send a new observation of a different type than the previous one", "Finish"]
 keyboard_classify = ['Classify a new one', "Finish"]
-keyboard_photo = ['This lamppost is of a different type than the previous one', "This lamppost is the same type as the previous one"]
 keyboard_color_day = ['white','orange']
-keyboard_color_night = ['HPS', 'LPS', 'LED', 'MV', 'MH']
+keyboard_color_night = ['High Pressure Sodium', 'Low Pressure Sodium', 'Light-Emitting Diode Lamps', 'Mercury Vapor', 'Metal Halide', 'No spectra']
 
 def handle_updates(updates):
     message=''
@@ -147,21 +153,30 @@ def handle_photo(message):
         logging.error("No location point")
     logging.info(download_url)
 
-    user_id = message["from"]["id"]
-    if user_id in observations:
-        observation = observations[user_id]
-        observation["image_url"] = download_url
-        observation["date"] = message["date"]
-        observations[user_id] = observation
-    else:
-        observations[user_id] = {"image_url": download_url+str(file_id)+".jpg","date":message["date"]}
-
-    send_message("Thank you!", chat)
-
     try:
 
+        response_image, headers = urllib.request.urlretrieve(download_url, str(file_id)+".jpg")
+        logging.info(response_image)
+
+        uploaded = upload_to_aws(response_image, 'tfm-telegram-bot', str(message["date"]) +".jpg")
+
+        final_url = BUCKET_BASE + str(message["date"]) + ".jpg"
+
+        logging.info(final_url)
+
+        user_id = message["from"]["id"]
+        if user_id in observations:
+            observation = observations[user_id]
+            observation["image_url"] = final_url
+            observation["date"] = str(message["date"])
+            observations[user_id] = observation
+        else:
+            observations[user_id] = {"image_url": final_url, "date": str(message["date"])}
+
+        send_message("Thank you!", chat)
+
         time_zone = observations[user_id]["time_zone"]
-        response_image = urllib.request.urlretrieve(download_url, str(file_id)+".jpg")
+
         #send_message("Your image has been registered in our system", chat)
         
         if time_zone == "Day":
@@ -169,7 +184,7 @@ def handle_photo(message):
             send_message("What is the color of the light?", chat,keyboard)
         elif time_zone == "Night":
             keyboard = build_reply_keyboard(keyboard_color_night)
-            send_message("What is the type of light?", chat,keyboard)
+            send_message("What is the light source associated to this spectra?", chat,keyboard)
 
 
     except Exception as e:
@@ -178,6 +193,9 @@ def handle_photo(message):
 
 #TODO: Asumimos que el usuario nos compartió su realtime. Si no lo hace o caduca, estaremos cogiendo
 # la última siempre hasta que vuelva a decir un Hi
+# SOLUCION: Guardar el timestamp del ultimo edited_message y que cuando nos llegue la siguiente observación que no sea de envio a la API, revisar que han pasado más de 5 minutos (poner como parametro)
+# desde el timestamp del editedmessage y si es asi , recordarle
+# que nos mande la ubicacion en tiempo real. 
 
 def handle_location(message, realtime):
     chat = message["chat"]["id"]
@@ -209,28 +227,34 @@ def handle_location(message, realtime):
 
     logging.info("location changed:"+str(observations[user_id]))
 
-def send_observation(user):
+def send_observation(user, classification):
 
     logging.info("------- NEW OBSERVATION ------")
     logging.info(",".join(("{}={}".format(*i) for i in observations[user].items())))
 
-    #TODO: Descomentar para enviar a API
-
-    #observation = {"datasource": "telegram", "userid": user, "observation": observations[user]}
+    if classification == 1:
+        observation = {"project": "street-spectra", "datasource": "telegram", "action": "classify", "userid": user, "observation": observations[user]}
+    else:
+        observation = {"project": "street-spectra", "datasource": "telegram", "action": "observe", "userid": user, "observation": observations[user]}
     
-    #response = requests.post("https://api.actionproject.eu/observations", json=observation)
+    header = {'Authorization': 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpYXQiOjE1ODk5NzEyOTgsIm5iZiI6MTU4OTk3MTI5OCwianRpIjoiYTE5MzM2MTUtZmQ5NS00ODFlLWJmY2YtMjkyYTUxZDRiNGU0IiwiZXhwIjoxNTkyNTYzMjk4LCJpZGVudGl0eSI6IlBydWViYSIsImZyZXNoIjpmYWxzZSwidHlwZSI6ImFjY2VzcyJ9.ZJrBt9R0v0ZcKc9kI_6jwFCbRZECvmM6h24jafPx7m8'}
 
-    #logging.info(response)
+    response = requests.post("https://api.actionproject.eu/observations", json=observation, headers=header)
 
-def get_observation(user):
+    logging.info(response)
 
-    response = requests.get("https://api.actionproject.eu/observations")
+def get_observation():
+
+    response = requests.get("https://api.actionproject.eu/observations?project=street-spectra")
 
     response_json = json.loads(response.text)
 
     logging.info(response_json)
 
-    rand_num = randrange(len(response_json))
+    rand_num = random.randrange(len(response_json))
+
+    logging.info("RANDNUM")
+    logging.info(rand_num)
 
     return response_json[rand_num]
 
@@ -238,7 +262,7 @@ def handle_text(update):
         try:
             text = update["message"]["text"]
             chat = update["message"]["chat"]["id"]
-            date = update["message"]["date"]
+            date = str(update["message"]["date"])
             first_name = update["message"]["from"]["first_name"]
             user_id = update["message"]["from"]["id"]
 
@@ -251,6 +275,7 @@ def handle_text(update):
             if 'hi' == text.lower():
                 keyboard = build_keyboard(keyboard_menu)
                 send_message('Hi.',chat, keyboard)
+                send_message('By continuing you accept that the following personal data: your Telegram ID and the location (latitude and longitude) of your device when sending each observation, will be available to the public and will be used for research purposes within the ACTION STREET SPECTRA Project.',chat, keyboard)
                 send_message('How can I help you?', chat, keyboard)
 
             if 'status' in text:
@@ -272,21 +297,50 @@ def handle_text(update):
                 #keyboard = json.dumps(reply_markup)
                 send_message("Share your realtime location with us by clicking in the clip button.",chat)
 
+            if 'Privacy policy' in text:
+
+                send_message("You can check our Privacy Policy at https://actionproject.eu/privacy-policy/",chat)
+
             #Only send night photos for classifying  
             if 'Classify' in text:
                 keyboard = build_keyboard(keyboard_color_night)
                 send_message('I will send you a photo for you to classify it', chat)
 
-                #TODO: revisar forma de acceder a la respuesta en funcion de la estructura de las observaciones
-                #to_classify = get_observation()["observation"]
-                #photo_url = to_classify['image']
+                # Esto sirve en el caso de que recibamos observaciones heterogéneas, no es el caso puesto que filtro por project street-spectra, que son las mias
+                # while "observation" in to_classify == False:
+                #     to_classify = get_observation()
 
-                to_classify = {"latitude": "40.416011","longitude": "-3.672166", "time_zone":"Night", "color": "HPS", "image_url": "https://streetspectra.actionproject.eu/wp-content/uploads/2020/02/action-street-spectra-5.jpeg" , "date":"1593951233", "classification": "yes"}
-                photo_url = "https://streetspectra.actionproject.eu/wp-content/uploads/2020/02/action-street-spectra-5.jpeg"
+                # to_classify = to_classify["observation"]
+
+                # logging.info("Classify 1/2 - ")
+                # logging.info(to_classify)
+
+                # while ("image_url" in to_classify) == False or ("latitude" in to_classify) == False or ("longitude" in to_classify) == False:
+                #     to_classify = get_observation()["observation"]
+                #     logging.info("Classify 2/2 - ")
+                #     logging.info(to_classify)
+
+                to_classify = get_observation()["observation"]
+
+                #TODO: QUITAR CUANDO BORREMOS PRUEBAS DE PROJECT TFM EN LA BASE DE DATOS
+                while ("s3" not in to_classify['image_url']) == True or ("1595001962" in to_classify['image_url']) == True:
+                    to_classify = get_observation()["observation"]
+                #QUITAR HASTA AQUI
                 
-                send_photo("What is the type of light?", photo_url, chat, keyboard)
 
-                #TODO: estoy enviando las clasificaciones con el mismo formato de las observaciones con un POST y cambiando el user_id y el color que tenian
+                photo_url = to_classify['image_url']
+
+                to_classify["time_zone"] = "Night"
+                to_classify["date"] = date
+                to_classify["classification"] = "yes"
+
+                logging.info(photo_url)
+
+                #{"latitude": "40.416011","longitude": "-3.672166", "time_zone":"Night", "color": "High Pressure Sodium", "image_url": "https://streetspectra.actionproject.eu/wp-content/uploads/2020/02/action-street-spectra-5.jpeg" , "date":"1593951233", "classification": "yes"}
+                #photo_url = "https://streetspectra.actionproject.eu/wp-content/uploads/2020/02/action-street-spectra-5.jpeg"
+                
+                send_photo("What is the light source associated to this spectra?", photo_url, chat, keyboard)
+
                 if user_id in observations:
                     observation = observations[user_id]
                     observation = to_classify
@@ -314,32 +368,27 @@ def handle_text(update):
             if 'Tip 1' in text:
                 keyboard = build_keyboard(keyboard_tutorial_step2)
                 send_message("I will start by sending you photos taken during the day", chat, keyboard)
-                send_photo("This is a white lamppost", 'ss-telegram-bot-master/assets/WHITE.png', chat)
-                send_photo("And this is an orange one",'ss-telegram-bot-master/assets/ORANGE.png', chat)
+                send_photo("This is a white lamppost", TUTORIAL_URL + 'WHITE.png?raw=true', chat)
+                send_photo("And this is an orange one", TUTORIAL_URL + 'ORANGE.png?raw=true', chat)
 
             if 'Tip 2' in text:
                 keyboard = build_keyboard(keyboard_tutorial_finish)
                 send_message("Now I will send you photos taken during the night", chat, keyboard)
-                send_photo("This is a HPS lamppost", 'ss-telegram-bot-master/assets/HPS.png', chat)
-                send_photo("This is a LPS lamppost", 'ss-telegram-bot-master/assets/LPS.png', chat)
-                send_photo("This is a LED lamppost", 'ss-telegram-bot-master/assets/LED.png', chat)
-                send_photo("This is a MV lamppost", 'ss-telegram-bot-master/assets/MV.png', chat)
-                send_photo("And finally, this is a MH lamppost", 'ss-telegram-bot-master/assets/MH.png', chat)
+                send_photo("This is a High Pressure Sodium lamppost", TUTORIAL_URL + 'HPS.png?raw=true', chat)
+                send_photo("This is a Low Pressure Sodium lamppost", TUTORIAL_URL + 'LPS.png?raw=true', chat)
+                send_photo("This is a Light-Emitting Diode Lamps lamppost", TUTORIAL_URL + 'LED.png?raw=true', chat)
+                send_photo("This is a Mercury Vapor lamppost", TUTORIAL_URL + 'MV.png?raw=true', chat)
+                send_photo("And finally, this is a Metal Halide lamppost", TUTORIAL_URL + 'MH.png?raw=true', chat)
 
-            if ('Exit' or 'Finish') in text:
+            if 'Exit' in text:
                 keyboard = build_keyboard(keyboard_menu)
                 send_message('How can I help you?', chat, keyboard)
-
-            if 'Send a new one' in text:
-                
-                keyboard = build_keyboard(keyboard_photo)
-                send_message("Choose an option when you find a new lamppost.",chat, keyboard)
 
             if 'Finish' in text:
                 del observations[user_id]
                 send_message("See you soon! Say Hi whenever you want to send new observations.",chat)
 
-            if 'This lamppost is of a different type than the previous one' in text:
+            if 'Send a new observation of a different type than the previous one' in text:
 
                 time_zone = observations[user_id]["time_zone"]
                 
@@ -349,24 +398,25 @@ def handle_text(update):
                     send_message("Awesome, place your grating in front of the camera and take a photo.",chat)
 
                 
-            if 'This lamppost is the same type as the previous one' in text:
+            if 'Send a new observation of the same type as the previous one' in text:
 
-                send_observation(user_id)
+                send_observation(user_id, 0)
                 keyboard = build_keyboard(keyboard_new)
                 send_message("Your observation has been registered", chat)
                 send_message("Do you want to send a new one?", chat, keyboard)
 
 
             if text == 'white' or text =='orange':
+
                 observation = observations[user_id]
                 observation["color"] = text
                 observations[user_id] = observation
-                send_observation(user_id)
+                send_observation(user_id, 0)
                 keyboard = build_keyboard(keyboard_new)
                 send_message("Your observation has been registered", chat)
                 send_message("Do you want to send a new one?", chat, keyboard)
 
-            if text == 'HPS' or text =='LPS' or text =='LED' or text =='MH' or text =='MV':
+            if text == 'High Pressure Sodium' or text =='Low Pressure Sodium' or text =='Light-Emitting Diode Lamps' or text =='Metal Halide' or text =='Mercury Vapor' or text =='No spectra':
 
                 observation = observations[user_id]
 
@@ -379,7 +429,7 @@ def handle_text(update):
                 observation["color"] = text               
                 observations[user_id] = observation
 
-                send_observation(user_id)
+                send_observation(user_id, classification)
                 send_message("Your observation has been registered", chat)
 
                 if classification == 1:
@@ -393,7 +443,7 @@ def handle_text(update):
         except Exception as e:
             logging.error(e)
             try:
-                date = update["message"]["date"]
+                date = str(update["message"]["date"])
                 chat = update["message"]["chat"]["id"]
                 send_message('I do not understand your message',chat)
 
@@ -435,6 +485,21 @@ def build_reply_keyboard(items):
     keyboard = [[item] for item in items]
     reply_markup = {"keyboard":keyboard, "one_time_keyboard": True, "force_reply": True}
     return json.dumps(reply_markup)
+
+def upload_to_aws(local_file, bucket, s3_file):
+    s3 = boto3.client('s3', aws_access_key_id=ACCESS_KEY,
+                      aws_secret_access_key=SECRET_KEY)
+
+    try:
+        s3.upload_file(local_file, bucket, s3_file, ExtraArgs={"Metadata": {"Content-Type":"iage/jpeg"}, 'ACL':'public-read'})
+        print("Upload Successful")
+        return True
+    except FileNotFoundError:
+        print("The file was not found")
+        return False
+    except NoCredentialsError:
+        print("Credentials not available")
+        return False
 
 
 def main():
